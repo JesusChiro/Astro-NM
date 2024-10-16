9f07912002ba729e3bdb2672cc380fa0acb52959
JesusChiro <jesus_chiroque@hotmail.com>
2024-10-16 14:11:53 -0500

    Changing bg-color, adding divider, color primary

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,13 +5,14 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'primary': '#333',
+				'primary': '#FFFAFA',
 				'secondary': '#fff',
 				'logonmt': '#4AC7F1',
 				'twitch': '#6441a4',
 				'logodark': '#0A195F',
 				'fondolight': '#0091E7',
-				
+				'fondodark': '#252626',
+
 			}
 		},
 	},
