01d6514617cee7b70dd160242302f9cc15673a1c
JesusChiro <jesus_chiroque@hotmail.com>
2024-11-11 18:27:28 -0500

    Apply Seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,30 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://nmtelecom.pe",
+  integrations: [tailwind(), sitemap(), robotsTxt(), purgecss({
+    fontFace: true,
+    keyframes: true,
+    safelist: ['random', 'yep', 'button', /^nav-/],
+    blocklist: ['usedClass', /^nav-/],
+    content: [
+      process.cwd() + '/src/**/*.{astro,vue}' // Watching astro and vue sources (for SSR, read the note below)
+    ],
+    extractors: [
+      {
+        // Example using a taiwindcss compatible class extractor
+        extractor: (content) =>
+          content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+        extensions: ['astro', 'html']
+      }
+    ]
+  })]
 });
