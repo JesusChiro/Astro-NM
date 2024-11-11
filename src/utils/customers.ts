83264ec1342847a92467476d8aaaa57698910404
JesusChiro <jesus_chiroque@hotmail.com>
2024-11-11 12:35:57 -0500

    add arrays for customers and providers

--- src/utils/customers.ts
+++ src/utils/customers.ts
@@ -1,17 +1,32 @@
-export const CUSTOMERS: Record<string, { name: string }> = {
-  vulco: {
-    name: "Vulco Perú",
-  },
-  incor: {
-    name: "Instituto Nacional Cardiológico",
-  },
-  unp: {
-    name: "Universidad Nacional de Piura",
-  },
-  pj: {
+export const CUSTOMERS = [
+  {
+    id: "poder-judicial-lima-norte",
     name: "Poder Judicial Lima Norte",
+    url: "https://www.gob.pe/csjlimanorte",
+    alt: "Cliente de NM TELECOM - Poder Judicial Lima Norte ",
   },
-  essalud: {
+  {
+    id: "instituto-nacional-del-corazon",
+    name: "Instituto Nacional del Corazon",
+    url: "http://campovirtualincor.essalud.gob.pe/campus_incor/",
+    alt: "Cliente de NM TELECOM - Instituto Nacional Cardiológico",
+  },
+  {
+    id: "weir-minerals-peru",
+    name: "Weir Mineral Perú - VULCO Perú",
+    url: "https://www.global.weir/es/",
+    alt: "Cliente de NM TELECOM - Weir Mineral Perú - VULCO Perú",
+  },
+  {
+    id: "essalud",
     name: "Seguro Social de Salud",
+    url: "https://www.essalud.gob.pe/essalud-en-linea/",
+    alt: "Cliente de NM TELECOM - Seguro Social de Salud",
   },
-};
+  {
+    id: "universidad-nacional-de-piura",
+    name: "Universidad Nacional de Piura",
+    url: "https://www.unp.edu.pe/unp/",
+    alt: "Cliente de NM TELECOM - Universidad Nacional de Piura ",
+  },
+];
---
+++ src/utils/providers.ts
@@ -0,0 +1,30 @@
+export const PROVIDERS = [
+  {
+    id: "anixter",
+    name: "Anixter || Wesco",
+  },
+  {
+    id: "fyco",
+    name: "Fyco Congloser",
+  },
+  {
+    id: "intcomex",
+    name: "Intcomex",
+  },
+  {
+    id: "nexus",
+    name: "Nexus Technology",
+  },
+  {
+    id: "gam",
+    name: "GAM Rentals",
+  },
+  {
+    id: "perfection",
+    name: "Perfection",
+  },
+  {
+    id: "sego",
+    name: "SEGO",
+  },
+];
