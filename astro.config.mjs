64efbf7522e200ca93d5d6a88cdb881bfe6dad88
JesusChiro <jesus_chiroque@hotmail.com>
2024-11-11 19:51:15 -0500

    delete purge css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,26 +6,10 @@ import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
 
-import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nmtelecom.pe",
-  integrations: [tailwind(), sitemap(), robotsTxt(), purgecss({
-    fontFace: true,
-    keyframes: true,
-    safelist: ['random', 'yep', 'button', /^nav-/],
-    blocklist: ['usedClass', /^nav-/],
-    content: [
-      process.cwd() + '/src/**/*.{astro,vue}' // Watching astro and vue sources (for SSR, read the note below)
-    ],
-    extractors: [
-      {
-        // Example using a taiwindcss compatible class extractor
-        extractor: (content) =>
-          content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-        extensions: ['astro', 'html']
-      }
-    ]
-  })]
+  integrations: [tailwind(), sitemap(), robotsTxt(),
+  ]
 });
